bcc7e1eee1c5afe32f2e68b9dc0501c1b110838a
wuyong <1062073083@qq.com>
2018-07-04 18:49:59 +0800

    update project worktime, but didnt finish

--- workTime/workTime/workTime.cpp
+++ workTime/workTime/workTime.cpp
@@ -13,11 +13,10 @@ using namespace std;
 
 void main()
 {
-/*	int N;      //一共有n条打卡纪录
+	int N;                //一共有n条打卡纪录
 	vector<int > dayRecord[32];
 	int workTime[32];
-	cin >> N;
-	if (getchar() == '\n');     //把键盘缓冲区的回车数据读走
+	cin >> N;            //会舍弃回车符
 	for (int i = 0; i < N; ++i)
 	{
 		int day = 0, hour = 0, min = 0, sec = 0;
@@ -25,15 +24,16 @@ void main()
 		getline(cin, line);
 		sscanf(line.c_str(), "08.%d %d:%d:%d", &day, &hour, &min, &sec);
 		int time = hour * 60 * 60 + min * 60 + sec;
-		if (time < START);
+		if (time < START)
+		{
+			dayRecord[day - 1].push_back(time);
+		}
+		else
+		{
+			dayRecord[day].push_back(time);
+		}
 	}
 
-	*/
-	int a = 0xf0;
-	int b = 0xf1;
-	int c = 1 << ((a & b) ^ (a | b));
-	cout << c << endl;
-	system("pause");
-
+	
 }
 
