ccc51a0324b408b7dde7c07c002d94c5df131fe7
wuyong <1062073083@qq.com>
2018-07-06 16:59:37 +0800

    update project worktime, but there were still some problem

--- workTime/workTime/workTime.cpp
+++ workTime/workTime/workTime.cpp
@@ -1,7 +1,9 @@
+//目前改题还是有些问题，稍后解决
 #include <iostream>
 #include <stdio.h>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,18 +13,19 @@ using namespace std;
 #define END 86400           //60*60*24，
 #define REST 5400           //REST_END - REST_START
 
-void main()
+int main()
 {
 	int N;                //一共有n条打卡纪录
 	vector<int > dayRecord[32];
 	int workTime[32];
-	cin >> N;            //会舍弃回车符
+	cin >> N;            //不会舍弃回车符
+	char c = getchar();
 	for (int i = 0; i < N; ++i)
 	{
 		int day = 0, hour = 0, min = 0, sec = 0;
 		string line;
 		getline(cin, line);
-		sscanf(line.c_str(), "08.%d %d:%d:%d", &day, &hour, &min, &sec);
+		sscanf_s(line.c_str(), "08.%d %d:%d:%d", &day, &hour, &min, &sec);
 		int time = hour * 60 * 60 + min * 60 + sec;
 		if (time < START)
 		{
@@ -33,8 +36,31 @@ void main()
 			dayRecord[day].push_back(time);
 		}
 	}
+	for (int i = 1; i < 32; ++i)         //
+	{
+		sort(dayRecord[i].begin(), dayRecord[i].end());
+		for (unsigned int j = 2; j <= dayRecord[i].size(); j = j + 2)
+		{
+			if (dayRecord[i][j] > REST_START && dayRecord[i][j] < REST_END)
+			{
+				workTime[i] += (REST_START - dayRecord[i][j - 1]);
+			}
+			else if (dayRecord[i][j - 1] > REST_START && dayRecord[i][j - 1] < REST_END)
+			{
+				workTime[i] += (dayRecord[i][j] - REST_END);
+			}
+			else
+			{
+				workTime[i] += (dayRecord[i][j] - dayRecord[i][j - 1]);
+			}
+		}
 
-
-	
+	}
+	for (int i = 1; i < 32; ++i)
+	{
+		printf("day%d : %d s\n", i, workTime[i]);
+	}
+	system("pause");
+	return 0;
 }
 
